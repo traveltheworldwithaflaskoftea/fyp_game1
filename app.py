e7a43247455001f723182d3b4ccddd4a3d4d44fc
traveltheworldwithaflaskoftea <65762500+traveltheworldwithaflaskoftea@users.noreply.github.com>
2022-03-04 00:14:21 +0800

    Integrate flask app w HTML page

--- app.py
+++ app.py
@@ -16,13 +16,35 @@ def test():
 
 @app.route('/')
 def firstpage():
-    return render_template('pick_char.html')
+    return render_template('1_index.html'), 200
 
+#Index page 
+
+# def getAllLifestages(): 
+
+#Select goal page
+
+#Questionaire page 
+@app.route('/questionaire')
+def getAllQuestions(): 
+    try:
+        questions = mongo.db.questionaire.find()
+        return render_template("3_questionaire.html", questions = questions), 200
+
+        #Check for correct extraction from DB
+        # questions = list(mongo.db.questionaire.find())
+        # return JSONEncoder().encode(questions), 200
+    except Exception as e: 
+        return dumps({'error': str(e)})
+
+#Tolerance Page 
+
+#Recommend Page
 @app.route('/recommendation')
 def getAllPortfolios(): 
     try:
         portfolios = mongo.db.portfolio.find()
-        return render_template("recommend.html", portfolios = portfolios), 200
+        return render_template("5_rec.html", portfolios = portfolios), 200
     except Exception as e: 
         return dumps({'error': str(e)})
 
@@ -30,7 +52,7 @@ def getAllPortfolios():
 def getOnePortfolio(id): 
     try:
         portfolio = mongo.db.portfolio.find_one_or_404({"_id": id})
-        return render_template("recommend.html", portfolio = portfolio), 200
+        return render_template("5_rec.html", portfolio = portfolio), 200
     except Exception as e: 
         return dumps({'error': str(e)})
 
