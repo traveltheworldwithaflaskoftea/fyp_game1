df44e0593e1726b9be552b89e895d8cdae3ed1d9
traveltheworldwithaflaskoftea <65762500+traveltheworldwithaflaskoftea@users.noreply.github.com>
2022-04-10 16:31:39 +0800

    Questionnaire HTTP Post and Gets

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from json import JSONEncoder
 from flask import Flask, jsonify, request, redirect, render_template
 from flask_pymongo import PyMongo
+from pymongo import ReturnDocument
+from utils import JSONEncoder
 from flask_cors import CORS
 from bson.json_util import dumps
 import json
@@ -9,6 +11,8 @@ app = Flask(__name__)
 CORS(app)
 app.config["MONGO_URI"] = "mongodb+srv://fyp1:RacH3nBu9ER2NW2o@clusterfyp.dwacg.mongodb.net/Game1?retryWrites=true&w=majority"
 mongo = PyMongo(app)
+# Need to make user dynamic !! and save it here 
+user = "user1"
 
 @app.route('/test')
 def test():
@@ -39,7 +43,6 @@ def getOneLifestage(id):
     try:
         lifestage = mongo.db.lifestage.find_one_or_404({"_id": id})
         return render_template("2_goal.html", lifestage = lifestage), 200
-        #return JSONEncoder().encode(goals), 200
     except Exception as e: 
         return dumps({'error': str(e)})
 
@@ -56,10 +59,44 @@ def getAllQuestions():
     except Exception as e: 
         return dumps({'error': str(e)})
 
+#Send risk tolerance & sustainability indication
+@app.route('/storeResult', methods=['POST'])
+def storeResult():
+    # Simple check of input format and data of the request are JSON
+    if request.is_json:
+        try:
+            result = request.get_json()
+            print("\nReceived result in JSON:",
+                  result)
+            data = mongo.db.user.find_one_and_update(
+                {"_id": user},
+                {"$set": result},
+                return_document=ReturnDocument.AFTER,
+            )
+            return JSONEncoder().encode(data), 200
+
+        except Exception as e:
+            return jsonify({
+                "code": 404,
+                "message": str(e)
+            }), 404
+
+    # if reached here, not a JSON request.
+    return jsonify({
+        "code": 400,
+        "message": "Invalid JSON input: " + str(request.get_data()),
+        "test": str(request.is_json)
+    }), 400
+
 #Tolerance Page 
 @app.route('/result')
 def getResult(): 
-    return render_template('4_result.html'), 200
+    try:
+        user_data = mongo.db.user.find_one_or_404({"_id": user})
+        # return JSONEncoder().encode(user_data), 200
+        return render_template("4_result.html", user_data = user_data), 200
+    except Exception as e: 
+        return dumps({'error': str(e)})
 
 #Recommend Page
 @app.route('/recommendation')
---
+++ utils.py
@@ -0,0 +1,13 @@
+import json
+from bson import ObjectId
+from datetime import datetime, date
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, (datetime, date)):
+            return o.isoformat()
+        return json.JSONEncoder.default(self, o)
+
